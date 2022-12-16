d25ed4e2bbb65c237b1b832e919e3c326cbe0d35
ahanaf <ahanafabdullah9@gmail.com>
2022-12-17 00:58:20 +0600

    layout fixing

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,10 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
   const { toggle } = useContext(ThemeContext);
   return (
-    <div data-theme={toggle ? "halloween" : "cmyk"}>
+    <div
+      data-theme={toggle ? "halloween" : "cmyk"}
+      className="max-w-screen-lg mx-auto"
+    >
       <Navbar />
       <main className=" my-10 lg:mx-20">{children}</main>
       <Footer />
